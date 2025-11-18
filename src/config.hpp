4ee13af731304c3f20fee9e0a32d6e12a821f6c6
Vlvin <ghostv.work@gmail.com>
2025-11-18 12:20:15 +0400

    Colorchange

--- src/config.hpp
+++ src/config.hpp
@@ -7,14 +7,14 @@
 constexpr bool topbar = true;
 
 constexpr int paddingX = 10;
-constexpr int paddingY = 3;
+constexpr int paddingY = 0;
 
 // See https://docs.gtk.org/Pango/type_func.FontDescription.from_string.html
-constexpr const char* font = "JetBrains Mono Nerd Font 11";
+constexpr const char* font = "FiraCode Nerd Font 16";
 
 constexpr ColorScheme colorInactive = {Color(0x93, 0xa1, 0xa1), Color(0x00, 0x00, 0x00)};
-constexpr ColorScheme colorActive = {Color(0xee, 0xee, 0xee), Color(0x00, 0x00, 0x00)};
-constexpr const char* termcmd[] = {"foot", nullptr};
+constexpr ColorScheme colorActive = {Color(0xee, 0xee, 0xee), Color(0x99, 0x00, 0xbb)};
+constexpr const char* termcmd[] = {"st", nullptr};
 
 static std::vector<std::string> tagNames = {
 	"1", "2", "3",
